18b72a07dc7d9e7ae634c7dee5226d134689a810
Hardik Kumar <kumarhardik821@gmail.com>
2025-07-21 08:20:12 -0700

    started the main page.

--- frontend/Host/StoreDetails/storeDetail.js
+++ frontend/Host/StoreDetails/storeDetail.js
@@ -1,60 +1,19 @@
-// const { createElement } = require("react");
+document.getElementById('login').addEventListener('submit' ,async function log(e) {
+    e.preventDefault();
 
-// setting up address api
-document.getElementById('btn').addEventListener('click' , coordinate);
-async function coordinate() {
-    const address = document.getElementById("address").value;
-    const apiKey = process.env.apiKey;
-    const url = `https://api.opencagedata.com/geocode/v1/json?q=${encodeURIComponent(address)}&key=${apiKey}&limit=1`;
-
-    const res = await fetch(url);
-    const data = await res.json();
-    console.log(data);
-    
-    // what i wanna do is first i wanna give him suggestions and then save his selected location and data in my backend 
-
-    //giving suggestions
-
-
-    // first check on the length of address
-    if(address.length < 3){
-        document.getElementById('suggestions').innerHTML = "";
-        return;
+    const UserData = {
+        userName : this.userName.value,
+        userPass : this.userPass.value
     }
 
+    const res = await fetch('/path of backen' ,{
+        method : 'Post',
+        header : {
+            'Content - Type' : 'application/json'
+        },
+        body : JSON.stringify(UserData)
+    } )
 
-    // map all the suggestions provided by opencage api 
-    const suggestions = data.results.map(result => result.formatted);
-
-
-
-    // make the ul clear
-    const suggestionList = document.getElementById("suggestions");
-    suggestionList.innerHTML = "";
-
-
-
-
-
-    // applying for each loop on all the mapped suggestions 
-    suggestions.forEach(suggestion => {
-
-        //made the list inside unordered list
-        const li = document.createElement("li");
-
-
-        //write al the suggestins inside the list 
-        li.textContent = suggestion;
-
-        // this function is used to fill the list value inside the address box 
-        li.onclick = ()=>{
-            document.getElementById('address').innerHTML = suggestion;
-
-            //empty the list
-            suggestionList.innerHTML = "";
-        }
-
-        suggestionList.appendChild(li);
-    });
-
-}
+    const data = await res.json();
+    console.log(data);
+})
