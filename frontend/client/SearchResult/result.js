e17991b4c631cbdde4e520072881eb4678bc6507
Hardik kumar <kumarhardik821@gmail.com>
2025-08-28 09:09:30 -0700

    making cards with the provided stores details by the hosts.

--- frontend/client/SearchResult/result.js
+++ frontend/client/SearchResult/result.js
@@ -13,4 +13,40 @@ window.addEventListener("DOMContentLoaded" , (e)=>{
         }
     }
     loadResultDetails();
+
+
+    async function CardsDataLoad() {
+    const container = document.getElementById("results-container");
+    try {
+        const res = await fetch("/listing/listing")
+        const Stores = await res.json();
+        container.innerHTML = "";
+        Stores.forEach(store => {
+            const card = document.createElement("div")
+            card.classList.add("store-card");
+            card.innerHTML = `
+            <img src = "${store.imageUrl || 'images/placeholder.jpg'}" alt= "${store.storeName}" class = "StoreImage">
+            <div class = "StoreInfo">
+            <h3>${store.storeName}</h3>
+            <p><strong>Address:</strong>${store.address}</p>
+            <p><strong>Type:</strong>${store.storeType}</p>
+           <button class="view-btn" data-id="${store._id}">View Details</button>
+            </div>
+            `;
+            container.appendChild(card);
+
+            document.querySelectorAll(".view-btn").forEach(button => {
+                button.addEventListener("click", (e) => {
+                    const storeId = e.target.dataset.id;
+                    window.location.href = `/client/storeDetails.html?id=${storeId}`;
+                });
+            });
+            
+        });
+    } catch (error) {
+        console.error("Error loading store cards:", error);
+    }
+    }
+    CardsDataLoad();
+
 })
